e46a21da9adb2a5bf1b1d3a050bccb5211b6670e
aniket thakur <860aniket@gmail.com>
2025-06-11 17:30:11 +0530

    food

---
+++ src/AppRouter.tsx
@@ -0,0 +1,22 @@
+import { IonRouterOutlet } from '@ionic/react'
+import { IonReactRouter } from '@ionic/react-router'
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+const AppRouter = () => {
+  return (
+    <><IonReactRouter>
+      <IonRouterOutlet>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+      </IonRouterOutlet>
+    </IonReactRouter></>
+  )
+}
+
+export default AppRouter
